afabcdcbc73bd1bdac155932ea3a4ff7da821d03
Haldies Gerhardien Pasya <haldiespasya@gmail.com>
2025-06-10 11:18:46 +0700

    Add Dockerfile and fix for Cloud Run

--- app.py
+++ app.py
@@ -47,5 +47,7 @@ class Predict(Resource):
             return {'error': str(e)}, 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    import os
+    port = int(os.environ.get('PORT', 8080)) 
+    app.run(host='0.0.0.0', port=port, debug=False)
+
