be5a60dfd4933305fad4e06f6c89e8390fc530bd
Haldies Gerhardien Pasya <haldiespasya@gmail.com>
2025-06-12 18:54:58 +0700

    update label index

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ import numpy as np
 from io import BytesIO
 
 app = Flask(__name__)
-CORS(app)  # Mengizinkan permintaan dari domain berbeda
+CORS(app)  
 
 api = Api(app, doc='/docs')
 
 model = load_model('app/model/model.h5')
-classes = ['Normal', 'Oily', 'Dry']
+classes = ['Dry','Normal', 'Oily']
 
 upload_parser = api.parser()
 upload_parser.add_argument('image', location='files', type=FileStorage, required=True, help='Gambar wajah')
